5b3f7058fb3b204ef4d8544127406d0bf88c2c46
Sahil <sahil.kumar5729@gmail.com>
2022-01-07 15:04:58 +0530

    Add books app

--- config/settings.py
+++ config/settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # Local
+    "books.apps.BooksConfig"
 ]
 
 MIDDLEWARE = [
