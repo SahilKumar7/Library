64268fbca69cd062bb1d1f267c2873d7a4ce0a34
Sahil <sahil.kumar5729@gmail.com>
2022-01-07 15:40:12 +0530

    Add tests

--- books/tests.py
+++ books/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
+from django.urls import reverse
+from .models import Book
 
-# Create your tests here.
+class BookTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.book = Book.objects.create(
+            title="MADAO",
+            subtitle="The Road of Gintama",
+            author="Hasegawa Taizo",
+            isbn="1234567890123",
+        )
+
+    def test_book_content(self):
+        self.assertEqual(self.book.title, "MADAO")
+        self.assertEqual(self.book.subtitle, "The Road of Gintama")
+        self.assertEqual(self.book.author, "Hasegawa Taizo")
+        self.assertEqual(self.book.isbn, "1234567890123")
+    
+    def test_book_listview(self):
+        response = self.client.get(reverse("home"))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "The Road of Gintama")
+        self.assertTemplateUsed(response, "books/book_list.html")
